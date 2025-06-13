518a9ec7a5a83debcacae0ee196aea1958937aa1
Jan G. <jan.g@telenet.be>
2025-06-13 09:43:48 +0200

    Added PDF

--- Module3 [Extra exercises]/SK-Foundry/obj/Debug/net9.0/SK-Foundry.AssemblyInfo.cs
+++ Module3 [Extra exercises]/SK-Foundry/obj/Debug/net9.0/SK-Foundry.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SK-Foundry")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6b1b035a08afd6e5dcf5a37fff0673cfddb2cb10")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bb77553651d9f480d4abbe92bcf718678ae96eb5")]
 [assembly: System.Reflection.AssemblyProductAttribute("SK-Foundry")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SK-Foundry")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
